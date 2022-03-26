316c0659ec717d03f4cb6ca837e7eff85942bb37
cassiadev <37288667+cassiadev@users.noreply.github.com>
2022-03-26 18:08:22 +0900

    Utilization of LayoutModifier

--- app/src/main/java/com/example/kankennikyucompose/MainActivity.kt
+++ app/src/main/java/com/example/kankennikyucompose/MainActivity.kt
@@ -28,9 +28,13 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.kankennikyucompose.ui.theme.KankenNikyuComposeTheme
@@ -58,6 +62,42 @@ private fun MyApp() {
 //        PhotographerCard()
 //        Question()
         ScrollingList()
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaseLineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseLine = placeable[FirstBaseline]
+
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaseLineToTop.roundToPx() - firstBaseLine
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            // Where the composable are placed
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    KankenNikyuComposeTheme() {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    KankenNikyuComposeTheme() {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
 
