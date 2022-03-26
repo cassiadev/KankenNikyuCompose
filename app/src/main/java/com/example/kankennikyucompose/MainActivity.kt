07ea4d87c21382e8e3d6379ac9d1f7b28ebd7987
cassiadev <37288667+cassiadev@users.noreply.github.com>
2022-03-26 17:43:06 +0900

    ScrollingList with Coil

--- app/src/main/java/com/example/kankennikyucompose/MainActivity.kt
+++ app/src/main/java/com/example/kankennikyucompose/MainActivity.kt
@@ -8,10 +8,12 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -30,7 +32,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.kankennikyucompose.ui.theme.KankenNikyuComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,15 +61,51 @@ private fun MyApp() {
 }
 
 @Composable
-fun SimpleList() {
-    // We save the scrolling position with this state that can also
-    // be used to programmatically scroll the list
-    val scrollState = rememberScrollState()
-
-    Column(Modifier.verticalScroll(scrollState)) {
-        repeat(24) {
-            Text(text = "Item #$it")
+fun ScrollingList() {
+    val listSize = 100
+    // We save the scrolling position with this state
+    val scrollState = rememberLazyListState()
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+    
+    Row() {
+        Button(onClick = { 
+            coroutineScope.launch { 
+                // 0 is the first item index
+                scrollState.animateScrollToItem(0)
+            }
+        }) {
+            Text(text = "Scroll to the top")
         }
+        Button(onClick = { 
+            coroutineScope.launch { 
+                // listSize -1 is the last index of the list
+                scrollState.animateScrollToItem(listSize - 1)
+            }
+        }) {
+            Text(text = "Scroll to the end")
+        }
+    }
+
+    LazyColumn(state = scrollState) {
+        items(100) {
+            ImageListItem(it)
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
